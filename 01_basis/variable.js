c29bafaad69e6a14e269ca70f9611e05878cbc9f
Nitesh Vishwakarma <Nitesh7415663723@gmail.com>
2025-08-24 18:29:18 +0000

    done

---
+++ 01_basis/variable.js
@@ -0,0 +1,17 @@
+const accountID=14423
+let accountEmail='Nitesh12@gmail.com'
+let account_password='12345'
+accountcity='indore'
+
+//accountID=2 can not  be change
+
+//console.log(accountID); 
+accountEmail='Nitesh51@gmail.com'
+account_password='212345'
+accountcity='harda'
+
+console.table([accountID,accountEmail,account_password,accountcity]);
+
+/*
+Please Dont Use Var Because his block of scope 
+*/
