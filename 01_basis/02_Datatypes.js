e6b5ce4650b61c96042a2c64c187ff5d67098bb1
Nitesh Vishwakarma <Nitesh7415663723@gmail.com>
2025-08-24 19:05:27 +0000

    done

---
+++ 01_basis/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use Strict";// for treat all new js code new version
+
+
+//alert(3+3); //we are using node js
+
+console.log(3+
+    3); //code readability must be high
+
+    let name="Nitesh"
+    let age=19
+    let isloggedin=true
+    let state;
+
+    //number => 2power to 53
+    //bigint 
+    //string >= ""
+    // boolean = true and false
+    // null = standalone value
+    // undefined 
+    // symbol = unique identify 
+    console.log(typeof age); // number 
+    console.log(typeof null); //object
+    console.log(typeof undefined); // undefined
+    
+    
+    
+
