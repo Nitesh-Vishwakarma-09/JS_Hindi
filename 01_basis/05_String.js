fe667617478df337f5387b41a6e666080419b696
Nitesh Vishwakarma <Nitesh7415663723@gmail.com>
2025-08-25 18:46:46 +0000

    complete

---
+++ 01_basis/05_String.js
@@ -0,0 +1,26 @@
+const name="Nitesh " 
+const repo=5
+console.log(name + repo + "values");
+
+console.log(`Hello My name is ${name} and my repo is ${repo}`);
+// Hello My name is Nitesh and My Repo is 5
+const game=new String ("GtA");
+console.log(game[0]);//G
+
+
+console.log(game.length)//#3
+
+console.log(game.toUpperCase());// GTA
+console.log(game.charAt(2)); // A
+console.log(game.indexOf('t')); //1 
+
+const bb="    Nitesh   "
+console.log(bb); //"    Nitesh   "
+
+console.log(bb.trim()); //Nitesh
+
+const url="Https://Nitesh.com/Nitesh%20Web"
+
+console.log(url.replace("%","-")); // replace % with -
+
+
