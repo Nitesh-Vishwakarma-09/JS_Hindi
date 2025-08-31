fbcd74c5b4ec5b48e6d85d01750596910fab6db9
Nitesh Vishwakarma <Nitesh7415663723@gmail.com>
2025-08-31 14:08:54 +0000

    done

--- 01_basis/06_Number_maths.js
+++ 01_basis/06_Number_maths.js
@@ -4,3 +4,21 @@ console.log(score);
 const balance=new Number(100)
 console.log(balance);
 
+console.log(balance.toFixed(2)); //100.00
+ 
+const hundreds=100000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// MATHS ///////////////
+
+console.log(Math.abs(-5)); // convert to 5
+console.log(Math.round(4.2)); //convert to 4
+console.log(Math.ceil(4.2)); // convert to 5 because ceil always choose big number 
+
+console.log(Math.floor(4.9)); // convert to 4 
+
+console.log(Math.floor(Math.random()*10))
+
+
+
+
