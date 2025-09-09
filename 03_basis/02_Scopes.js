4de9a45a77d1261b62be13b3738f3f34b2785def
Nitesh Vishwakarma <Nitesh7415663723@gmail.com>
2025-09-09 18:04:28 +0000

    commit

--- 03_basis/02_Scopes.js
+++ 03_basis/02_Scopes.js
@@ -7,8 +7,17 @@ if(true)
 
    // console.log(a);  that not print because is defined in outside body that scope 
     // console.log(b); that not print because is defined in outside body that scope 
-    console.log(c); // but c is print because var is not scope that by var is not use java scripyt 
+   // console.log(c); // but c is print because var is not scope that by var is not use java scripyt 
     
+    //+++++++++++++++++++++++++ INTERESTING +++++++++++
+console.log(addone(5)); // in this simple we can access function in before declarition of function
+    function addone(num1){ // simple function declare example 
+        return num1 +1
     
+    }
+    console.log(b(5)); //Cannot access 'b' before initialization this type error 
     
+    const b=function two(num1){ // because this is used in mini hosting & this function declare in a variable 
+        return num+1
+    }
 
