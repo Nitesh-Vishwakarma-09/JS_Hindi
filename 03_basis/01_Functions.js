1be356da17ef068830899305c9d0d6914e51d251
Nitesh Vishwakarma <Nitesh7415663723@gmail.com>
2025-09-09 11:57:14 +0000

    commit

---
+++ 03_basis/01_Functions.js
@@ -0,0 +1,26 @@
+function SayMyName(){
+    console.log("N");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+    
+}
+ // SayMyName()
+ function addtwo(number1,number2){
+  //  console.log(number1 + number2);
+    let result=number1 + number2
+    return result
+ }
+//addtwo(3,5)
+
+const result=addtwo(3,5)
+console.log("Result ",result);
+
+function loginUserMessage(username){
+    return `${username} Just Logged In`
+}
+console.log(loginUserMessage("Nitesh"));
+
+
