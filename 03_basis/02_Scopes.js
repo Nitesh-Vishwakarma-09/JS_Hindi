39d7e23c185de071caf0e9e200c4a3972437f2c5
Nitesh Vishwakarma <Nitesh7415663723@gmail.com>
2025-09-09 12:27:20 +0000

    commit

---
+++ 03_basis/02_Scopes.js
@@ -0,0 +1,14 @@
+if(true) 
+    {
+    let a=10;
+    const b=20;
+    var c=30;
+    } //  that create scope that variable inside in currly braces 
+
+   // console.log(a);  that not print because is defined in outside body that scope 
+    // console.log(b); that not print because is defined in outside body that scope 
+    console.log(c); // but c is print because var is not scope that by var is not use java scripyt 
+    
+    
+    
+
