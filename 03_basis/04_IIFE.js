2f8b36e91dfb77e6cfdfa84286425d0882548d14
Nitesh Vishwakarma <Nitesh7415663723@gmail.com>
2025-09-12 18:03:58 +0000

    commit

---
+++ 03_basis/04_IIFE.js
@@ -0,0 +1,20 @@
+// Immediately Invoked Function Expression (IIFE)
+
+function DB(){
+    console.log('DATABASE CONNECTED');// simple function 
+}
+
+// IIFE FUNCTION is known as imediaetly invoked for Global Scope Pollution 
+
+(function DB1(){
+    console.log('DATABASECONNECTED'); 
+})() ;
+// to create IIFE Function Just NOrmal Function Repad in Pranthises () is called IIFE
+
+/// IIFE USING ARROW FUNCTION 
+
+( (name) => {
+    console.log(`DATABASE CONNECT ${name}`);
+})("Nitesh")  
+
+//--------------------------------------------------
