d153900a36f4a6516dfb4c47457562cfce573718
Nitesh Vishwakarma <Nitesh7415663723@gmail.com>
2025-09-13 19:26:52 +0000

    commit

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,15 @@
+const month=3;
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febaury");
+        break;
+    case 3:
+        console.log("March");
+        break; // this break is used to break to code that value matches the condition that we not used break that there will run all after condition matches execpt default code  
+
+    default:
+        break;
+}
