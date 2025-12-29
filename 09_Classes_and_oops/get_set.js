c5bb238828d93296546b3f46a28e15c4c82226c1
Nitesh Vishwakarma <Nitesh7415663723@gmail.com>
2025-12-29 19:35:21 +0000

    commit

---
+++ 09_Classes_and_oops/get_set.js
@@ -0,0 +1,22 @@
+class User{
+    constructor(email, password){
+        this.email = email
+        this.password = password
+    }
+     get password(){ // getter me koi apki class ki property access karta h toh apko kese dena h originally ya thodi modified means alag jese password pr get karega toh sharma automatically add ho jayega 
+    return `${this._password}sharma`
+    }
+     set password(value){
+     this._password = value
+    }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email =value
+    }
+}
+
+const Nitesh = new User("Nitesh@12gmail.com","nitesh1")
+console.log(Nitesh.email);
+
